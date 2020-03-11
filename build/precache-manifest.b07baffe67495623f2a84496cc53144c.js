72a195598856b420822be138c2df6d292403d53b
leeum <liam.tate@hotmail.com>
2020-03-11 13:41:32 -0400

    heroku build fix

---
+++ build/precache-manifest.b07baffe67495623f2a84496cc53144c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cddec1bd22387ca829c8",
+    "url": "/static/css/main.bc6ba81d.chunk.css"
+  },
+  {
+    "revision": "cddec1bd22387ca829c8",
+    "url": "/static/js/main.0156ee18.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ea7c8e50c69f3f07fe88",
+    "url": "/static/js/2.7c6f99f4.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "97a37d7944395a051ebe2dcd7046bcf8",
+    "url": "/index.html"
+  }
+];
