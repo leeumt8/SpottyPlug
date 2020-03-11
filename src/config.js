b6322b983a6be955b7415fb8e4ba0a398f4ad9eb
leeum <liam.tate@hotmail.com>
2020-03-11 13:21:13 -0400

    removed build

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-// Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "4ff33cf0a2bb43deb113db9f1faaee2d";
 export const redirectUri = "http://localhost:3000/redirect";
 export const scopes = [
