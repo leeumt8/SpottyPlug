91a194988610528cf17cb75e6ee421355c03db56
leeum <liam.tate@hotmail.com>
2020-03-11 13:53:06 -0400

    redirectURI tweaks

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 export const clientId = "4ff33cf0a2bb43deb113db9f1faaee2d";
-export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "https://spottyplug.herokuapp.com/";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
